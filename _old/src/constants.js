43b8dd4ea4d7e8eb38ac2c56e8fb06343d9e826c
mindsgn <seni.tembe@protonmail.com>
2023-10-16 15:50:37 +0200

    remove folder

--- _old/src/constants.js
+++
@@ -1,7 +0,0 @@
-const ImageButtons = [
-  { image: "assets/ducks.png", input: "" },
-  { image: "assets/fullducks.png", input: "" },
-  { image: "assets/hearts.png", input: "" },
-];
-
-export { ImageButtons };
